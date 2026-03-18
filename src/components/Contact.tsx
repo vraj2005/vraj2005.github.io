c07c2c16ff32896a6e670ec0fe6756efd0c6656a
Vraj Nakum <vrajnakum57@gmail.com>
2026-03-18 18:26:04 +0530

    first commit

---
+++ src/components/Contact.tsx
@@ -0,0 +1,69 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="mailto:rajeshchittyal21@gmail.com" data-cursor="disable">
+                rajeshchittyal21@gmail.com
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>BSc in Computer Science</p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/raxx21"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/rajesh-chityal-2a70141b3"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            <a
+              href="https://x.com/raxx21_official"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.instagram.com/therajeshchityal"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Instagram <MdArrowOutward />
+            </a>
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Rajesh Chityal</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2025
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
